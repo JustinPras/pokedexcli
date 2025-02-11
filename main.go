42bc4b5610ec3d28615bc7b642c1d5370e13fba9
JustinPras <justinponpras@gmail.com>
2025-02-11 22:44:13 +1100

    REPL

--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 	"strings"
+	"bufio"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		ok := scanner.Scan()
+		if !ok {
+			break
+		}
+		text := scanner.Text()
+		input := cleanInput(text)
+		fmt.Printf("Your command was: %v\n", input[0])
+	}
 }
 
 func cleanInput(text string) []string {
