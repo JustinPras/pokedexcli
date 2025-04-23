3c913f8d016b2e7dae7d2bcf12a89fff5f92ea08
JustinPras <justinponpras@gmail.com>
2025-04-23 15:33:56 +1000

    add unit test for help command

--- repl_test.go
+++ repl_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"os/exec"
 	"testing"
 )
 
@@ -32,5 +34,42 @@ func TestCleanInput(t *testing.T) {
 			}
 			
 		}
+	}
+}
+
+func TestHelpCommand(t *testing.T) {
+	cmd := exec.Command("go", "run", ".")
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &out
+
+
+	cmd.Stdin = bytes.NewReader([]byte("help\n"))
+	err := cmd.Run()
+	if err != nil {
+		t.Fatalf("Error running command: %v", err)
+	}
+
+
+	expectedOutput := 	`Pokedex > 
+Welcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+map: Display the next page of locations in the Pokemon world
+mapb: Display the previous page of locations in the Pokemon world
+explore <location_name>: Explore the Pokemon world
+catch <pokemon_name>: Throw a Pokeball at a pokemon
+inspect <pokemon_name>: Inspect a Pokemon from your Pokedex
+pokedex: Displays all Pokemon in your Pokedex
+exit: Exit the Pokedex
+
+Pokedex > `
+
+	actualOutput := out.String()
+
+	if actualOutput != expectedOutput {
+		t.Errorf("Help command output is incorrect.\nGot:\n%s\nExpected:\n%s", actualOutput, expectedOutput)
 	}
 }
